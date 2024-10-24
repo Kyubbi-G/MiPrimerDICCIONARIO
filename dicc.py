d2ea7afc1e5e4e8e03d6863feef2fd692a6638d5
Kyubbi-G <leticiaa9968@gmail.com>
2024-10-24 14:10:52 -0300

    Create dicc.py

---
+++ dicc.py
@@ -0,0 +1,13 @@
+None
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "WP": "buen jugo",
+            }
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("No esta esa palabra en el diccionario")
